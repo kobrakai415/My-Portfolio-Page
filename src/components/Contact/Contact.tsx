eeff85c74d92de3d1540a3add681eff04bfcad9d
kobrakai415 <kai.kadir@hotmail.co.uk>
2021-09-28 16:58:18 +0100

    added links

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Row } from 'react-bootstrap';
+
+const Contact = () => {
+    return (
+        <Row id="contact">
+
+        </Row>
+    );
+}
+
+export default Contact;
