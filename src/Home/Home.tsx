83f0c959e3a17dc139ef171995a6e7705e0829ee
kobrakai415 <kai.kadir@hotmail.co.uk>
2021-09-29 22:36:23 +0100

    linkedin github links

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,31 +1,33 @@
 import React from 'react';
-import {Col } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import './Home.css'
 
 const Home = () => {
     return (
 
+        <Row>
 
-        <div id="home" className="home p-5 d-flex flex-column ">
-            <h1 className="name text-center">Kaiwan Kadir</h1>
+            <div id="home" className="home p-5 d-flex flex-column ">
+                <h1 className="name text-center">Kaiwan Kadir</h1>
 
-            <div className="d-flex flex-column">
+                <div className="d-flex flex-column">
 
-                <div>
-                    <p className="mb-0">Welcome to my portfolio.</p>
-                    <div className="d-flex justify-content-center">
-                        <img className="img-fluid" src="/me.png" />
+                    <div>
+                        <p className="mb-0">Welcome to my portfolio.</p>
+                        <div className="d-flex justify-content-center">
+                            <img className="img-fluid" src="/me.png" />
+                        </div>
+                    </div>
+                    <div >
+                        <div className="mx-5 d-flex justify-content-center py-5">
+                            <p style={{ width: "500px" }}>I make full stack web applications and have a background in central government finance, having worked in whitehall for 4 years!</p>
+                        </div>
                     </div>
                 </div>
-                <div >
-                <div className="mx-5 d-flex justify-content-center py-5">
-                    <p style={{width: "500px"}}>I make full stack web applications and have a background in central government finance, having worked in whitehall for 4 years!</p>
-                </div>
-                </div>
+
             </div>
 
-        </div>
-
+        </Row>
     );
 }
 
--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -48,18 +48,18 @@ const About = () => {
 
                     <div>
 
-                        <img className="p-1" src='https://img.shields.io/badge/html%20-%23150458.svg?&style=for-the-badge&logo=html&logoColor=white' />
-                        <img className="p-1" src='https://img.shields.io/badge/CSS%20-%23F37626.svg?&style=for-the-badge&logo=CSS&logoColor=white' />
-                        <img className="p-1" src='https://img.shields.io/badge/JavaScript-ED8B00?style=for-the-badge&logo=javascript&logoColor=white' />
-                        <img className="p-1" src="https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white" />
-                        <img className="p-1" src="https://img.shields.io/badge/Express.js-404D59?style=for-the-badge" />
-                        <img className="p-1" src='https://img.shields.io/badge/MongoDB-14354C?style=for-the-badge&logo=mongodb&logoColor=white' />
-                        <img className="p-1" src='https://img.shields.io/badge/PostgreSQL-00000F?style=for-the-badge&logo=postgresql&logoColor=white' />
-                        <img className="p-1" src='https://img.shields.io/badge/React-00599C?style=for-the-badge&logo=react&logoColor=white' />
-                        <img className="p-1" src="https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white" />
-                        <img className="p-1" src='https://img.shields.io/badge/typescript%20-%23013243.svg?&style=for-the-badge&logo=typescript&logoColor=white' />
-                        <img className="p-1" src="https://img.shields.io/badge/Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white" />
-                        <img className="p-1" src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" />
+                        <img className="p-1" alt="logo" src='https://img.shields.io/badge/html%20-%23150458.svg?&style=for-the-badge&logo=html&logoColor=white' />
+                        <img className="p-1" alt="logo" src='https://img.shields.io/badge/CSS%20-%23F37626.svg?&style=for-the-badge&logo=CSS&logoColor=white' />
+                        <img className="p-1" alt="logo" src='https://img.shields.io/badge/JavaScript-ED8B00?style=for-the-badge&logo=javascript&logoColor=white' />
+                        <img className="p-1" alt="logo" src="https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white" />
+                        <img className="p-1" alt="logo" src="https://img.shields.io/badge/Express.js-404D59?style=for-the-badge" />
+                        <img className="p-1" alt="logo" src='https://img.shields.io/badge/MongoDB-14354C?style=for-the-badge&logo=mongodb&logoColor=white' />
+                        <img className="p-1" alt="logo" src='https://img.shields.io/badge/PostgreSQL-00000F?style=for-the-badge&logo=postgresql&logoColor=white' />
+                        <img className="p-1" alt="logo" src='https://img.shields.io/badge/React-00599C?style=for-the-badge&logo=react&logoColor=white' />
+                        <img className="p-1" alt="logo" src="https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white" />
+                        <img className="p-1" alt="logo" src='https://img.shields.io/badge/typescript%20-%23013243.svg?&style=for-the-badge&logo=typescript&logoColor=white' />
+                        <img className="p-1" alt="logo" src="https://img.shields.io/badge/Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white" />
+                        <img className="p-1" alt="logo" src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" />
 
                     </div>
 
